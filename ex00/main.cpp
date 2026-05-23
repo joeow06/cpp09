478239083d20cb02664fc5569a19881ee3abfbbf
joeow06 <joeow06@gmail.com>
2026-05-24 00:28:33 +0800

    feat: added program main

---
+++ ex00/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jow <jow@student.42kl.edu.my>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 10:24:54 by jow               #+#    #+#             */
+/*   Updated: 2026/05/19 10:24:56 by jow              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Error: invalid parameters" << std::endl;
+		return (1);
+	}
+	try
+	{
+		(void)argv;
+		BitcoinExchange btc("data.csv");
+	}
+	catch (const std::exception &e)
+	{
+
+	}
+}
